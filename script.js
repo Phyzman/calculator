fbc4aa839f05b7a7052f83cb417f02c617ba5fe5
unknown <dafeez242@gmail.com>
2024-10-21 09:07:28 +0100

    calculator

---
+++ script.js
@@ -0,0 +1,89 @@
+
+
+let inputDigit = document.getElementById('inputNumber');
+
+function clickOne(){
+inputDigit.value += '1';
+}
+
+function clickTwo(){
+inputDigit.value+= '2';
+  
+}
+function clickThree(){
+inputDigit.value+= '3';
+  
+}
+function clickFour(){
+inputDigit.value+= '4';
+  
+}
+function clickFive(){
+inputDigit.value+= '5';
+  
+}
+function clickSix(){
+inputDigit.value+= '6';
+  
+}
+function clickSeven(){
+inputDigit.value+= '7';
+  
+}
+function clickEight(){
+inputDigit.value+= '8';
+  
+}
+function clickNine(){
+inputDigit.value+= '9';
+  
+}
+function clickZero(){
+inputDigit.value+= '0';
+  
+}
+function clickDoubleZero(){
+inputDigit.value+= '00';
+  
+}
+function clickPercent(){
+inputDigit.value+= '%';
+
+}
+function clickMinus(){
+inputDigit.value+= '-';
+
+}
+function clickDivision(){
+inputDigit.value+= '/';
+
+}
+function clickMultiply(){
+inputDigit.value+= '*';
+
+}
+
+
+function clearDigit(){
+inputDigit.value = '';
+  
+}
+
+function clickPlus(){
+inputDigit.value+= "+";
+  
+}
+
+function clickComma(){
+inputDigit.value+= ',';
+  
+}
+
+function clickEval(){
+    inputDigit.value = eval(inputDigit.value);
+}
+function clickMinus(){
+    inputDigit.value+= '-'
+}
+
+
